0603c858ccb494eec5972d4cd8ef809533768455
Kevin McQuown <kevin@thewcl.com>
2020-06-08 16:57:31 -0500

    initial commit

--- pd6LocationServices/ViewController.swift
+++ pd6LocationServices/ViewController.swift
@@ -1,20 +1,50 @@
-//
-//  ViewController.swift
-//  pd6LocationServices
-//
-//  Created by Kevin McQuown on 6/8/20.
-//  Copyright © 2020 Kevin McQuown. All rights reserved.
-//
-
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        checkForLocationServices()
     }
 
-
+    func checkForLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            checkForAppLocationServices()
+        } else {
+            // notify user that they must turn on location services
+        }
+    }
+    
+    func checkForAppLocationServices() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+   //         locationManager.startMonitoring(for: <#T##CLRegion#>)
+            map.showsUserLocation = true
+        case .authorizedAlways:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .denied:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+    }
 }
-
