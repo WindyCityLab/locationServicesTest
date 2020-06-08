3113f63fc552b4b0dbdf3e210b8dfa4539a78d6f
Kevin McQuown <kevin@thewcl.com>
2020-06-08 09:24:48 -0500

    Initial Commit

---
+++ pd6LocationServices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pd6LocationServices
+//
+//  Created by Kevin McQuown on 6/8/20.
+//  Copyright © 2020 Kevin McQuown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pd6LocationServicesTests/pd6LocationServicesTests.swift
@@ -0,0 +1,34 @@
+//
+//  pd6LocationServicesTests.swift
+//  pd6LocationServicesTests
+//
+//  Created by Kevin McQuown on 6/8/20.
+//  Copyright © 2020 Kevin McQuown. All rights reserved.
+//
+
+import XCTest
+@testable import pd6LocationServices
+
+class pd6LocationServicesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
